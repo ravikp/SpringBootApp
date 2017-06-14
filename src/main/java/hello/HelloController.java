70a0677d4a79a2cb1498c9277fe10a59797e7933
Ravi Kumar Pasumarthy <ravikp@users.noreply.github.com>
2017-06-14 17:33:08 +0530

    Implement http endpoint root  to display greetings with current server time

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings.." + new Date() + System.lineSeparator();
+    }
+}
--- src/main/java/hello/Init.java
+++ src/main/java/hello/Init.java
@@ -1,7 +1,12 @@
 package hello;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Init {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        SpringApplication.run(Init.class, args);
     }
 }
+
