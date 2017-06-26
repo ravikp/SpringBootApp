ff872acfa82b177a9cc9f5e1aae4fc9df262dc55
Ravi Kumar Pasumarthy <ravikp@users.noreply.github.com>
2017-06-27 01:21:41 +0530

    Remove custom '/health' endpoint, since the endpoint is available in SpringBoot

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -11,10 +11,5 @@ class HelloController {
     @RequestMapping("/")
     public String index() {
         return "Greetings from fully automated deployment system:" + new Date() + System.lineSeparator();
-    }
-
-    @RequestMapping("/health")    
-    public String health() {
-        return "I'm healthy at:" + new Date() + System.lineSeparator();
-    }
+    }    
 }
